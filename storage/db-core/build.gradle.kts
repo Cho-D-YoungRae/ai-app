088b1412c577508fb33cdec64ec647b06279f2a1
Youngrae Cho <yrc9229@gmail.com>
2025-03-27 11:07:15 +0900

    유저 엔티티 추가

--- storage/db-core/build.gradle.kts
+++ storage/db-core/build.gradle.kts
@@ -5,6 +5,8 @@ allOpen {
 }
 
 dependencies {
+    implementation(project(":core:core-enum"))
+
     // JPA
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
---
+++ storage/db-core/src/main/kotlin/com/assignment/aiapp/storage/db/core/UserEntity.kt
@@ -0,0 +1,27 @@
+package com.assignment.aiapp.storage.db.core
+
+import com.assignment.aiapp.core.enums.UserRole
+import jakarta.persistence.*
+
+@Table(
+    name = "users",
+    uniqueConstraints = [
+        UniqueConstraint(name = "uk_users__email", columnNames = ["email"])
+    ]
+)
+@Entity
+class UserEntity(
+    @Column(name = "email", nullable = false)
+    val email: String,
+
+    @Column(name = "password", nullable = false)
+    val password: String,
+
+    @Column(name = "name", length = 25, nullable = false)
+    val name: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", length = 25, nullable = false)
+    val role: UserRole = UserRole.MEMBER
+): BaseEntity() {
+}
