2a7e919bfd9f6c855d4851965861588b32a882b0
Youngrae Cho <yrc9229@gmail.com>
2025-03-27 11:21:48 +0900

    회원가입 API 누락된 request body 추가

--- core/core-api/src/main/kotlin/com/assignment/aiapp/api/controller/v1/UserController.kt
+++ core/core-api/src/main/kotlin/com/assignment/aiapp/api/controller/v1/UserController.kt
@@ -2,7 +2,9 @@ package com.assignment.aiapp.api.controller.v1
 
 import com.assignment.aiapp.api.controller.v1.request.UserSignupRequest
 import com.assignment.aiapp.domain.UserService
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,7 +15,7 @@ class UserController(
 ) {
 
     @PostMapping("/signup")
-    fun signup(request: UserSignupRequest) {
+    fun signup(@RequestBody @Validated request: UserSignupRequest) {
         userService.signup(request.toUserSignup())
     }
 }
